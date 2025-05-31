62a44d8112727ae96306e05b7febb071c93b2f6f
Happy2018new <chenbo79800@163.com>
2025-05-31 17:39:27 +0800

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/Happy2018new/the-last-problem-of-the-humankind
+
+go 1.24
+
+require (
+	github.com/go-gl/mathgl v1.2.0
+	github.com/go-jose/go-jose/v3 v3.0.4
+	github.com/google/uuid v1.6.0
+	github.com/ugorji/go/codec v1.2.14
+	golang.org/x/text v0.25.0
+)
+
+require (
+	github.com/golang/snappy v1.0.0
+	github.com/klauspost/compress v1.18.0
+	github.com/muhammadmuzzammil1998/jsonc v1.0.0
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0
+)
---
+++ minecraft/protocol/packet/sync_actor_property.go
@@ -0,0 +1,21 @@
+package packet
+
+import (
+	"github.com/Happy2018new/the-last-problem-of-the-humankind/minecraft/nbt"
+	"github.com/Happy2018new/the-last-problem-of-the-humankind/minecraft/protocol"
+)
+
+// SyncActorProperty is an alternative to synced actor data.
+type SyncActorProperty struct {
+	// PropertyData ...
+	PropertyData map[string]any
+}
+
+// ID ...
+func (*SyncActorProperty) ID() uint32 {
+	return IDSyncActorProperty
+}
+
+func (pk *SyncActorProperty) Marshal(io protocol.IO) {
+	io.NBT(&pk.PropertyData, nbt.NetworkLittleEndian)
+}

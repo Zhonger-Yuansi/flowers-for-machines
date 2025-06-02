be43c150dacc64c553a66559620f54204e577e5b
Happy2018new <chenbo79800@163.com>
2025-06-02 09:43:43 +0800

    fix sync map

--- utils/sync_map.go
+++ utils/sync_map.go
@@ -88,7 +88,7 @@ func (m *SyncMap[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
 func (m *SyncMap[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
 	act, loaded := m.mapping.LoadOrStore(key, value)
 	if !loaded {
-		return actual, false
+		return act.(V), false
 	}
 	return act.(V), true
 }

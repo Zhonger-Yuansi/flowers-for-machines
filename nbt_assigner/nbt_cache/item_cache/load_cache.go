40a41a83257d748b5be4f95255b9bd29b0db45d5
Happy2018new <chenbo79800@163.com>
2025-06-07 17:32:29 +0800

    minor enhance on the item cache system

--- nbt_assigner/nbt_cache/item_cache/load_cache.go
+++ nbt_assigner/nbt_cache/item_cache/load_cache.go
@@ -51,26 +51,3 @@ func (i *ItemCache) LoadCache(hashNumber ItemHashNumber, exclusion []resources_c
 	}
 	return
 }
-
-// ConsumeCache 使 slotID 所指示的物品被消耗，
-// 这使底层认为 slotID 所指示的槽位可以被重用
-func (i *ItemCache) ConsumeCache(slotID resources_control.SlotID) {
-	newOne := make([]ItemCacheInfo, 0)
-
-	for _, value := range i.firstCache {
-		if value.SlotID == slotID {
-			continue
-		}
-		newOne = append(newOne, value)
-	}
-
-	i.firstCache = newOne
-	i.console.SetInventorySlot(slotID, false)
-}
-
-// CleanInventory 将背包中所有物品都标记为空气，
-// 这使得整个背包的所有物品栏都将可用被重新使用
-func (i *ItemCache) CleanInventory() {
-	i.firstCache = nil
-	i.console.CleanInventory()
-}

c2af1c4f628288b88c78700c648f26d880367970
Happy2018new <chenbo79800@163.com>
2025-06-11 01:40:29 +0800

    remove unnecessary files

--- nbt_assigner/nbt_item/default_item.go
+++
@@ -1,9 +0,0 @@
-package nbt_item
-
-import (
-	"github.com/Happy2018new/the-last-problem-of-the-humankind/nbt_assigner/nbt_cache/nbt_block_cache"
-)
-
-type DefaultItem struct {
-	cache *nbt_block_cache.NBTBlockCache
-}

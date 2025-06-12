81fde82e56e8fb5ead82fe7ba0ca7c39d1df085b
Happy2018new <chenbo79800@163.com>
2025-06-12 13:18:42 +0800

    minor enhance

--- nbt_assigner/nbt_assigner.go
+++ nbt_assigner/nbt_assigner.go
@@ -1,6 +1,7 @@
 package nbt_assigner
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/Happy2018new/the-last-problem-of-the-humankind/core/minecraft/protocol"
@@ -35,8 +36,12 @@ func NewNBTAssigner(
 	}
 }
 
-// PlaceNBTBlock 制作一个新的 NBT 方块 nbtBlock。
-// 制作位置是操作台的中心方块处。
+// PlaceNBTBlock 试图制作一个新的 NBT 方块，
+// 制作位置是在操作台的中心方块处。
+//
+// blockName 是要制作的 NBT 方块的方块名称；
+// blockStates 是要制作的 NBT 方块的方块状态；
+// blockNBT 是要制作的 NBT 方块的方块实体数据。
 //
 // canFast 指示目标方块是否可以直接通过 setblock 放置。
 //
@@ -46,14 +51,20 @@ func NewNBTAssigner(
 //
 // PlaceNBTBlock 是阻塞的，它保证同一时刻只会制作一个
 // NBT 方块
-func (n *NBTAssigner) PlaceNBTBlock(nbtBlock nbt_parser_interface.Block) (
+func (n *NBTAssigner) PlaceNBTBlock(blockName string, blockStates map[string]any, blockNBT map[string]any) (
 	canFast bool,
 	uniqueID uuid.UUID,
 	offset protocol.BlockPos,
 	err error,
 ) {
+	nbtBlock, err := nbt_parser_interface.ParseBlock(blockName, blockStates, blockNBT)
+	if err != nil {
+		return false, uuid.UUID{}, protocol.BlockPos{}, fmt.Errorf("PlaceNBTBlock: %v", err)
+	}
+
 	n.mu.Lock()
 	defer n.mu.Unlock()
+
 	canFast, uniqueID, offset, err = nbt_assigner_interface.PlaceNBTBlock(n.console, n.cache, nbtBlock)
 	return
 }
